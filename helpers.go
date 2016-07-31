400c50c55819d9bb1307411d5a50ead4a930a702
nathanaelle <nathanaelle@users.noreply.github.com>
2016-07-31 18:32:47 +0200

    add helpers for Human readable representation of metric value

--- helpers.go
+++ helpers.go
@@ -1,8 +1,95 @@
 package	types	// import "github.com/nathanaelle/useful.types"
 
 import	(
+	"math"
+	"strconv"
 )
 
+
+var 	humanscale_exp_byte	[]byte = []byte { 'y','z','a','f','p','n','µ','m',' ','k','M','G','T','P','E','Z','Y' }
+
+
+func Round(x float64) int {
+	switch	{
+	case	x < 0:
+		return int(x-0.5)
+	case	x > 0:
+		return int(x+0.5)
+	default:
+		return 0
+	}
+}
+
+
+func MetricPower(value float64, base float64, max_precision int) (neg bool, scaled float64, precision int, power int) {
+	power_f	:= math.Floor(math.Log2(value)/math.Log2(base))
+	power	= int(power_f)
+	scaled	= value / math.Pow(base, power_f)
+	if value < 0 {
+		neg	= true
+	}
+
+	if max_precision <= 0 {
+		return
+	}
+
+	_,fracf	:= math.Modf(scaled)
+	frac	:= Round(fracf*math.Pow(10,float64(max_precision)))
+	if frac == 0 {
+		return
+	}
+	precision= max_precision
+
+	for precision > 0 {
+		if (frac % 10) != 0 {
+			break
+		}
+		frac = Round(float64(frac)/10)
+		precision--
+	}
+
+	return
+}
+
+
+func HumanScaleString(value float64, base float64, unit string) string {
+	if value == 0 {
+		return	"0"+unit
+	}
+
+	exp	:= []string { "y","z","a","f","p","n","µ","m","","k","M","G","T","P","E","Z","Y" }
+	_, h_v, p, s	:= MetricPower(value, base, 3)
+
+	if s > -9 && s < 9 {
+		return	strconv.FormatFloat(h_v,'f',p,64)+exp[int(s)+8]+unit
+	}
+
+	return strconv.FormatFloat(value,'E',6,64)+unit
+}
+
+func HumanScaleBytes(value float64, base float64, unit []byte) []byte {
+	if value == 0 {
+		return	append( append(make([]byte,0,1+len(unit)), '0'), unit...)
+	}
+
+	neg, h_v, p, s	:= MetricPower(value, base, 3)
+	sign	:= 0
+	if neg {
+		sign	= 1
+	}
+
+	switch {
+	case	s == 0:
+		return	append( strconv.AppendFloat(make([]byte,0,sign+4+p+len(unit)), h_v,'f',p,64), unit...)
+	case s > -9 && s < 9 :
+		return	append(	append(
+			strconv.AppendFloat(make([]byte,0,sign+4+p+1+len(unit)), h_v,'f',p,64),
+			humanscale_exp_byte[int(s)+8]),
+			unit...)
+	default:
+		return	append( strconv.AppendFloat(make([]byte,0,sign+12+len(unit)), value,'E',6,64), unit...)
+	}
+}
 
 
 
---
+++ helpers_test.go
@@ -0,0 +1,55 @@
+package	types	// import "github.com/nathanaelle/useful.types"
+
+import (
+	"testing"
+	"bytes"
+)
+
+type	hsb_test struct {
+	value	float64
+	base	float64
+	unit	[]byte
+	result	[]byte
+}
+
+
+func Test_HumanScale(t *testing.T) {
+	tests := []hsb_test {
+		{ 0, 1000, []byte("m"), []byte("0m") },
+		{ 1000, 1000, []byte("m"), []byte("1km") },
+		{ 1000000, 1000, []byte("m"), []byte("1Mm") },
+		{ 0.001, 1000, []byte("m"), []byte("1mm") },
+	}
+
+	for i,cas := range tests {
+		res_b	:= HumanScaleBytes(cas.value, cas.base, cas.unit)
+		if !bytes.Equal(cas.result, res_b) {
+			t.Errorf("t_%02d : [%v] expected, got [%v]", i, cas.result, res_b)
+		}
+		res_s	:= HumanScaleString(cas.value, cas.base, string(cas.unit))
+
+		if res_s != string(cas.result) {
+			t.Errorf("t_%02d : [%v] expected, got [%v]", i, string(cas.result), res_s)
+		}
+
+
+	}
+
+
+
+}
+
+
+func Benchmark_HumanScaleBytes(b *testing.B) {
+	unit	:= []byte("iB")
+	for i := 0; i < b.N; i++ {
+		HumanScaleBytes(13573471044894720, 1024, unit)
+	}
+}
+
+
+func Benchmark_HumanScaleString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		HumanScaleString(13573471044894720, 1024, "iB")
+	}
+}
